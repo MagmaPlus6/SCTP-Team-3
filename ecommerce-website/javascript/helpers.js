ea4f05f82c17cb86510e3f7c57b421032989164e
Mattn4 <nky_88@hotmail.com>
2023-12-20 23:57:26 +0800

    Added interactivity for order cart page

--- ecommerce-website/javascript/helpers.js
+++ ecommerce-website/javascript/helpers.js
@@ -17,6 +17,22 @@ const getAllProducts = async () => {
     }
 }
 
+// Function to get single product
+const getSingleProduct = async (id) => {
+    try {
+        const response = await fetch(`http://localhost:8080/products/${id}`);
+
+        if(response.ok) {
+            const jsonResponse = await response.json();
+            // console.log(`Product with id ${id}`, jsonResponse);
+            return jsonResponse;
+        }
+        // throw new Error('Request failed!');
+    } catch (error) {
+        console.log('Error:', error);
+    }
+}
+
 // Function to save product
 const saveProduct = async (data) => {
     try {
@@ -99,4 +115,103 @@ const sortAllProductsByPriceDescend = async () => {
     } catch (error) {
         console.log('Error:', error);
     }
+}
+
+// Add product to shopping cart 
+// NOT WORKING
+
+const addProductToShoppingCart = (data) => {
+
+    const orderList = document.querySelector('.order-list')
+
+	console.log('orderList: ' + orderList)
+
+	if(orderList) {
+
+	const subOrderList = document.createElement("div");
+	subOrderList.className = "row border-bottom m-1 py-2 align-items-center";
+
+	subOrderList.innerHTML = `
+
+		<div class="col-lg-4 col-md mb-4 mb-lg-0 text-center">
+			<img
+				src="image/${data.image}"
+				class="img-fluid shopping-cart-img"
+				alt=""
+			/>
+		</div>
+
+		<div class="col-lg-8 col-md mb-4 mb-lg-0">
+			<p><strong>${data.name}</strong></p>
+
+			<div class="form-group row">
+				<label for="size" class="col-lg-3 col-md-5 col-sm-3 col-form-label">Size</label>
+				<div class="col-lg col-md-8 col-sm">
+					<div class="input-group">
+						<div id="radioBtn${data.id}" class="btn-group">
+							<a class="btn btn-primary btn-sm active" data-toggle="size${data.id}" data-title="XS">XS</a>
+							<a class="btn btn-primary btn-sm notActive" data-toggle="size${data.id}" data-title="S">S</a>
+							<a class="btn btn-primary btn-sm notActive" data-toggle="size${data.id}" data-title="M">M</a>
+							<a class="btn btn-primary btn-sm notActive" data-toggle="size${data.id}" data-title="L">L</a>
+							<a class="btn btn-primary btn-sm notActive" data-toggle="size${data.id}" data-title="XL">XL</a> 
+						</div>
+						<input type="hidden" name="size${data.id}" id="size${data.id}">
+					</div>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label for="quantity" class="col-lg-3 col-md-6 col-sm-3 col-form-label">Quantity</label>
+
+				<div class="col-lg-5 col-md-8 col-sm-4">
+					<div class="input-group number-spinner">
+						<span class="input-group-btn btn-light">
+							<button class="btn btn-default" data-dir="dwn">
+								<span class="fas fa-minus"></span>
+							</button>
+						</span>
+						<input type="text" class="form-control text-center quantity-input" value="1">
+						<span class="input-group-btn btn-light">
+							<button class="btn btn-default" data-dir="up">
+								<span class="fas fa-plus"></span>
+							</button>
+						</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-3 col-md-5 col-sm-3">
+					<p>Price</p>
+				</div>
+				<div class="col-md-8 col-sm">
+					<p class="">
+						<strong>$${data.price}</strong>
+					</p>
+				</div>   
+			</div>
+
+			<button
+				type="button"
+				class="btn btn-danger btn-sm mb-2"
+				data-mdb-toggle="tooltip"
+				title="Move to the wish list"
+			>
+				<i class="fas fa-heart"></i>
+			</button>
+			<button
+				type="button"
+				class="btn btn-secondary btn-sm mb-2 float-right"
+				data-mdb-toggle="tooltip"
+				title="Remove item"
+			>
+				<i class="fas fa-trash"></i>
+			</button>
+		</div>
+	`
+
+	orderList.appendChild(subOrderList);
+
+	}
+
 }
--- ecommerce-website/javascript/ordercart.js
+++ ecommerce-website/javascript/ordercart.js
@@ -1,4 +1,27 @@
-// $('#radioBtn a').on('click', function(){
+
+// Update in Shopping cart summary section
+const shoppingCartSummary = () => {
+
+    let pricesOfProduct = document.querySelectorAll('.price')
+
+    let sum = 0;
+
+    pricesOfProduct.forEach(i => {
+        sum += parseFloat(i.innerText);
+    });
+
+    const subtotal = document.querySelector('.subtotal')
+    subtotal.innerHTML = sum.toFixed(2)
+
+    const shippingFee = document.querySelector('.shipping-fee').innerHTML
+
+    const total = document.querySelector('.total')
+    total.innerHTML = (parseFloat(subtotal.innerHTML) + parseFloat(shippingFee)).toFixed(2)
+}
+
+
+// Select product size
+// $('.btn-group a').on('click', function(){
 //     var sel = $(this).data('title');
 //     var tog = $(this).data('toggle');
 //     $('#'+tog).prop('value', sel);
@@ -7,29 +30,84 @@
 //     $('a[data-toggle="'+tog+'"][data-title="'+sel+'"]').removeClass('notActive').addClass('active');
 // })
 
-$('.btn-group a').on('click', function(){
-    var sel = $(this).data('title');
-    var tog = $(this).data('toggle');
-    $('#'+tog).prop('value', sel);
-    
-    $('a[data-toggle="'+tog+'"]').not('[data-title="'+sel+'"]').removeClass('active').addClass('notActive');
-    $('a[data-toggle="'+tog+'"][data-title="'+sel+'"]').removeClass('notActive').addClass('active');
-})
-
-
-$(document).on('click', '.number-spinner button', function () {    
-	var btn = $(this),
-		oldValue = btn.closest('.number-spinner').find('input').val().trim(),
-		newVal = 0;
-	
-	if (btn.attr('data-dir') == 'up') {
-		newVal = parseInt(oldValue) + 1;
-	} else {
-		if (oldValue > 1) {
-			newVal = parseInt(oldValue) - 1;
-		} else {
-			newVal = 1;
-		}
-	}
-	btn.closest('.number-spinner').find('input').val(newVal);
+document.querySelectorAll('.btn-group a').forEach(function (element) {
+	element.addEventListener('click', function () {
+	  var sel = this.getAttribute('data-title');
+	  var tog = this.getAttribute('data-toggle');
+	  document.getElementById(tog).value = sel;
+  
+	  document.querySelectorAll('a[data-toggle="'+tog+'"]:not([data-title="'+sel+'"])').forEach(function (el) {
+		el.classList.remove('active');
+		el.classList.add('notActive');
+	  });
+  
+	  this.classList.remove('notActive');
+	  this.classList.add('active');
+	});
 });
+  
+
+// Change product quantity
+// $(document).on('click', '.number-spinner button', function () {    
+// 	var btn = $(this),
+// 		oldValue = btn.closest('.number-spinner').find('input').val().trim(),
+// 		newVal = 0;
+
+// 	const originalPrice = btn.parents('.col-lg-8.col-md.mb-4').find('.price').attr('data')
+
+// 	if (btn.attr('data-dir') == 'up') {
+// 		newVal = parseInt(oldValue) + 1;
+// 	} else {
+// 		if (oldValue > 1) {
+// 			newVal = parseInt(oldValue) - 1;
+// 		} else {
+// 			newVal = 1;
+// 		}
+// 	}
+// 	btn.closest('.number-spinner').find('input').val(newVal);
+
+// 	btn.parents('.col-lg-8.col-md.mb-4').find('.price').text(parseFloat(originalPrice*newVal).toFixed(2))
+
+// 	shoppingCartSummary()
+// });
+
+document.addEventListener('click', function (event) {
+	var target = event.target;
+  
+	if (target.classList.contains('number-spinner') || target.closest('.number-spinner button')) {
+	  var btn = target.closest('.number-spinner button');
+	  var oldValue = btn.closest('.number-spinner').querySelector('input').value.trim();
+	  var newVal = 0;
+  
+	  var originalPrice = btn.closest('.col-lg-8.col-md.mb-4').querySelector('.price').getAttribute('data');
+  
+	  if (btn.getAttribute('data-dir') == 'up') {
+		newVal = parseInt(oldValue) + 1;
+	  } else {
+		if (oldValue > 1) {
+		  newVal = parseInt(oldValue) - 1;
+		} else {
+		  newVal = 1;
+		}
+	  }
+  
+	  btn.closest('.number-spinner').querySelector('input').value = newVal;
+  
+	  btn.closest('.col-lg-8.col-md.mb-4').querySelector('.price').textContent = (parseFloat(originalPrice) * newVal).toFixed(2);
+  
+	  shoppingCartSummary();
+	}
+});
+
+// Delete button
+document.querySelectorAll("button[title='Remove item']").forEach(function (element) {
+	element.addEventListener('click', function () {
+
+		const productToRemove = element.closest('.row.border-bottom.m-1.py-2.align-items-center')
+
+		productToRemove.remove();
+
+		shoppingCartSummary();
+	});
+});
+  
